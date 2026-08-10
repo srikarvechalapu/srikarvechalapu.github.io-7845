5a9566fa26feeed1883a24bf6174459a2a264a96
Srikar Vechalapu <vechalapusrikar25@gmail.com>
2026-08-10 11:54:06 -0400

    Update resume download link in main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -338,7 +338,7 @@
       bio.innerHTML = aboutData.bio.map(function (p) {
         return '<p>' + esc(p) + '</p>';
       }).join('') +
-      '<a href="assets/Srikar_Vechalapu_Data_Analyst_Resume.pdf" download class="btn-download-resume">' +
+      '<a href="assets/Srikar_Vechalapu_Data_Engineer%20(3).pdf" download="Srikar_Vechalapu_Data_Engineer_Resume.pdf" class="btn-download-resume">' +
         '<svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path><polyline points="7 10 12 15 17 10"></polyline><line x1="12" y1="15" x2="12" y2="3"></line></svg>' +
         '<span>Download Resume</span>' +
       '</a>';
